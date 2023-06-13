59907cd5b285f352440d156219cf7ad2ed29ee84
Alejandra Arias <alejandra.aarias0@gmail.com>
2023-06-13 00:27:23 -0500

    made intial updates

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,20 @@
-export default function Header({ title }) {
-  return <h1 className="title">{title}</h1>
+import React from "react";
+export default function Header() {
+  return(
+    <React.Fragment>
+        <header class="header-main">
+        <a href="/">
+          <img alt="header-gif" class="hrv float" src="ariasali\public\prog1.gif"></img>
+          </a>
+          <nav>
+            <ul className="ul-special">
+                <li ><a className="hvr-float org-color-text" href="/about">About</a></li>
+                <li><a className="hvr-float org-color-text" href="/projects">Projects</a></li>
+                <li><a className="hvr-float org-color-text" href="/contact">Contact</a></li>
+                <li><a className="hvr-float org-color-text" href="https://drive.google.com/file/d/1SKOvFQybQB0GQU9tsg30d2_W4o2KsjTc/view?usp=sharing">Resume</a></li>
+            </ul>
+          </nav>
+        </header>
+    </React.Fragment>
+  )
 }
