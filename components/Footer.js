bd435931d70414e022aad8e2513b08a5b15bbe21
Alejandra Arias <alejandra.aarias0@gmail.com>
2023-06-13 00:12:46 -0500

    made intial updates

--- components/Footer.js
+++ components/Footer.js
@@ -1,11 +1,19 @@
 import styles from './Footer.module.css'
-
+import React from "react";
 export default function Footer() {
   return (
-    <>
-      <footer className={styles.footer}>
-        <img src="/logo-netlify.svg" alt="Netlify Logo" className={styles.logo} />
+    <React.Fragment>
+      <footer>
+      <div>
+      <a href="https://github.com/ariasalejandra"><img class="hvr-float"alt="github" src="ariasali\images\github.svg"></img></a>
+      <a href="https://linkedin.com/in/aalejandra"><img class="hvr-float"alt="linkedin" src="ariasali/images/linkedin.svg"></img></a>
+      <a href="https://instagram.com/alejandra.arix"><img class="hvr-float"alt="instagram" src="ariasali/images/instagram.svg"></img></a>
+      <a href="mailto:alejandra.aarias0@gmail.com"><img class="hvr-float"alt="email" src="ariasali\images\email.svg"></img></a>
+  </div>
+  <div >
+    <p>Made with ‎♡ by alejandra / <a class="underline"href="https://github.com/ariasalejandra/">Source</a></p>
+  </div>
       </footer>
-    </>
+    </React.Fragment>
   )
 }
