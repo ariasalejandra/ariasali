c3bc09f36cb39581981feb4d7da171b0623b187f
Alejandra Arias <alejandra.aarias0@gmail.com>
2023-06-14 00:00:25 -0500

    added routes

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Projects from "components/projects"
 export default function Header() {
   return(
     <React.Fragment>
@@ -7,9 +8,9 @@ export default function Header() {
           </a>
           <nav>
             <ul className="ul-special">
-                <li ><a className="hvr-float org-color-text" href="/about">About</a></li>
-                <li><a className="hvr-float org-color-text" href="/projects">Projects</a></li>
-                <li><a className="hvr-float org-color-text" href="/contact">Contact</a></li>
+                <li ><a className="hvr-float org-color-text" href="components/about.js">About</a></li>
+                <li><a className="hvr-float org-color-text" href={Projects}>Projects</a></li>
+                <li><a className="hvr-float org-color-text" href="/contact.js">Contact</a></li>
                 <li><a className="hvr-float org-color-text" href="https://drive.google.com/file/d/1SKOvFQybQB0GQU9tsg30d2_W4o2KsjTc/view?usp=sharing">Resume</a></li>
             </ul>
           </nav>
