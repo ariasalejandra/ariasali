f93a8deccfef0b19e77cc58e53e5bad5fecada38
Alejandra Arias <alejandra.aarias0@gmail.com>
2023-06-14 00:00:00 -0500

    fixed footer

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ import Github from "/images/github.svg"
 export default function Footer() {
   return (
     <React.Fragment>
-      <footer classNAme={styles.footer}>
+      <footer className={styles.footer}>
       <div>
       <a href="https://github.com/ariasalejandra"><img classNAme={styles.footer} class="hvr-float"src="https://media.giphy.com/media/CwTvSiWflgCGKgz5eb/giphy.gif" width="30"></img></a>
       <a href="https://linkedin.com/in/aalejandra"><img classNAme={styles.footer} class="hvr-float"alt="linkedin" src="https://media.giphy.com/media/HQTYdpx1yhxWpugAi2/giphy.gif" width="30"></img></a>
@@ -13,7 +13,7 @@ export default function Footer() {
       <a href="https://github.com/ariasalejandra/ariasali"><img classNAme={styles.footer} class="hvr-float"src="https://media.giphy.com/media/kAm4u0lhDCmXnugz6p/giphy.gif"width="30"></img></a>
   </div>
   <div >
-    <p>Made with ‎♡ by alejandra</p>
+    <p className={styles.text}>Made with ‎♡ by alejandra</p>
   </div>
       </footer>
     </React.Fragment>
